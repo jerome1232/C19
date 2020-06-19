b3d6a84281fe6bd2d756a24cce6317c5a9223e8f
Jeremy D. Jones <j.jones1232@gmail.com>
2020-06-18 23:10:59 -0600

    added documentation

--- app/src/main/java/com/example/c19/GlobalCovid.kt
+++ app/src/main/java/com/example/c19/GlobalCovid.kt
@@ -1,5 +1,15 @@
 package com.example.c19
 
+/**
+ * Represents Global CVOID-19 Stats
+ *
+ * @property newConfirmed New confirmed cases today
+ * @property totalConfirmed Total confirmed cases
+ * @property newDeaths New deaths attributed to COVID-19 today
+ * @property totalDeaths Total deaths attributed to COVID-19
+ * @property newRecovered People that have recovered from COVID-19 Today
+ * @property totalRecovered Total people that have recovered from COVID-19
+ */
 data class GlobalCovid(
     val newConfirmed: Int,
     val totalConfirmed: Int,
