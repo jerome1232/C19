432fcb4247aa29e32637cf4964390c5aab3af369
Jeremy D. Jones <j.jones1232@gmail.com>
2020-06-19 09:16:32 -0600

    added data class country covid, replaced date fetched with one appropriate to min api level of 21. Previous method required api level 26

--- app/src/main/java/com/example/c19/CountryCovid.kt
+++ app/src/main/java/com/example/c19/CountryCovid.kt
@@ -1,4 +1,26 @@
 package com.example.c19
 
-class CountryCovid {
-}
+/**
+ * Represents COVID-19 data for a single country
+ *
+ * @author Jeremy D. Jones
+ *
+ * @property country
+ * @property countryCode
+ * @property slug
+ * @property date
+ * @property newConfirmed
+ * @property totalConfirmed
+ * @property totalDeaths
+ * @property totalRecovered
+ */
+data class CountryCovid(
+    val country: String,
+    val countryCode: String,
+    val slug: String,
+    val date: String,
+    val newConfirmed: Int,
+    val totalConfirmed: Int,
+    val totalDeaths: Int,
+    val totalRecovered: Int
+)
--- app/src/main/java/com/example/c19/GlobalCovid.kt
+++ app/src/main/java/com/example/c19/GlobalCovid.kt
@@ -3,12 +3,14 @@ package com.example.c19
 /**
  * Represents Global CVOID-19 Stats
  *
+ * @author Jeremy D. Jones
  * @property newConfirmed New confirmed cases today
  * @property totalConfirmed Total confirmed cases
  * @property newDeaths New deaths attributed to COVID-19 today
  * @property totalDeaths Total deaths attributed to COVID-19
  * @property newRecovered People that have recovered from COVID-19 Today
  * @property totalRecovered Total people that have recovered from COVID-19
+ * @property fetchedDate Datetime that data was fetched
  */
 data class GlobalCovid(
     val newConfirmed: Int,
@@ -17,4 +19,6 @@ data class GlobalCovid(
     val totalDeaths: Int,
     val newRecovered: Int,
     val totalRecovered: Int
-)
+) {
+    val fetchedDate = java.util.Calendar.getInstance()
+}
