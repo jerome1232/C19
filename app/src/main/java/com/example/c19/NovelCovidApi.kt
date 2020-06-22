61a365442e6122f204f26ae89f569f5ef6be013b
Jeremy D. Jones <j.jones1232@gmail.com>
2020-06-22 17:07:24 -0600

    retrofit interface errors resolved

--- app/src/main/java/com/example/c19/NovelCovidApi.kt
+++ app/src/main/java/com/example/c19/NovelCovidApi.kt
@@ -2,10 +2,11 @@ package com.example.c19
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
-private val url = "https://corona.lmao.ninja/v2/"
+//private val url = "https://corona.lmao.ninja/v2/"
+//https://corona.lmao.ninja/v2/states/:states?yesterday=true
 interface NovelCovidApi {
-    @GET("states?sort&yesterday")
-    Call<List<StateUsCovid>> getStates()
+    @GET("states/{state}")
+    fun getState(@Path("state") key: String): Call<StateUsCovid>
 }
