ae8331c67eee966cdcdedb55e294962ac35ef395
Jeremy D. Jones <j.jones1232@gmail.com>
2020-06-23 07:23:22 -0600

    Added permissions, adjusted some imports, added libraries and comments in gradle to make clear which libraries are added vs already there

--- app/src/main/java/com/example/c19/NovelCovidApi.kt
+++ app/src/main/java/com/example/c19/NovelCovidApi.kt
@@ -1,12 +1,29 @@
 package com.example.c19
 
 import retrofit2.Call
+import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 //private val url = "https://corona.lmao.ninja/v2/"
 //https://corona.lmao.ninja/v2/states/:states?yesterday=true
+
+
+/**
+ * @author Jeremy D. Jones
+ *
+ * This is an interface to facilitate reaching out to
+ * the various API's.
+ *
+ */
 interface NovelCovidApi {
     @GET("states/{state}")
     fun getState(@Path("state") key: String): Call<StateUsCovid>
+
+    companion object {
+        fun create(): NovelCovidApi {
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        }
+    }
 }
