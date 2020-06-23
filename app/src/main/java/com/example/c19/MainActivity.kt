2633a63459cf1a55f1b92d44eb719a7699098e22
chasemoses <43392487+chasemoses@users.noreply.github.com>
2020-06-22 19:00:40 -0600

    Fragments created. Can cycle through fragments in the navigation pane.

--- app/src/main/java/com/example/c19/MainActivity.kt
+++ app/src/main/java/com/example/c19/MainActivity.kt
@@ -5,47 +5,114 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.view.menu.MenuItemImpl
+import androidx.core.view.GravityCompat
 import androidx.core.view.MenuCompat
 import androidx.core.view.MenuItemCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.drawer_toolbar.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var compareFragment: CompareFragment
+    lateinit var historicalDataFragment: HistoricalDataFragment
+    lateinit var settingsFragment: SettingsFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Create variables for the different layouts and tie the layout id to them
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer);
-        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        navigationView.setNavigationItemSelectedListener(this)
 
-        // Remove the default name in the Toolbar
-/*        setSupportActionBar(toolbar)
-          supportActionBar?.setDisplayShowTitleEnabled(false)
-          val toolbarTitle = findViewById<TextView>(R.id.toolbar) // Create our own TextView in the toolbar
-          val mTitle = toolbarTitle.findViewById<TextView>(R.id.toolbar_title)
-
- */
         // This enables the 3 hamburger image at the top left of the toolbar
         val actionBarDrawerToggle = object : ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close) {}
 
         // Look for if the button is activated. If so, open the navigation pane
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.setDrawerIndicatorEnabled(true)
+        actionBarDrawerToggle.isDrawerIndicatorEnabled = true
         actionBarDrawerToggle.syncState()
+
+        // Remove the default name in the Toolbar
+/*        setSupportActionBar(toolbar)
+          supportActionBar?.setDisplayShowTitleEnabled(false)
+          val toolbarTitle = findViewById<TextView>(R.id.toolbar) // Use our own TextView in the toolbar
+          val mTitle = toolbarTitle.findViewById<TextView>(R.id.toolbar_title)
+
+ */
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_fragment, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+
+        // Logic for loading the default fragment upon app opening
+//        val fragmentManager : FragmentManager = getSupportFragmentManager()
+//        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.add(R.id.container_fragment, HomeFragment()) // Setting HomeFragment as default fragment
+//        fragmentTransaction.commit()
+
 
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
 
-        if(item.getItemId() == R.id.home) {
+        when(menuItem.itemId) {
+            R.id.home -> {
+                homeFragment = HomeFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container_fragment, homeFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
 
+            R.id.compare -> {
+                compareFragment = CompareFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container_fragment, compareFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+
+            R.id.historical -> {
+                historicalDataFragment = HistoricalDataFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container_fragment, historicalDataFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+
+            R.id.settings -> {
+                settingsFragment = SettingsFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container_fragment, settingsFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
         }
 
-
-
+        drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 }
