994ca34f5bccaee341130efb29f18cdfea4ca667
jerome1232 <j.jones1232@gmail.com>
2020-07-02 17:10:00 -0600

    modified to work with novel19 api as it's data better fits usecase.

--- app/src/main/java/com/example/c19/model/CountryCovid.kt
+++ app/src/main/java/com/example/c19/model/CountryCovid.kt
@@ -1,26 +1,36 @@
 package com.example.c19.model
 
+import com.google.gson.annotations.SerializedName
+
 /**
- * Represents COVID-19 data for a single country
+ * Represents Covid 19 data for a single country
  *
- * @author Jeremy D. Jones
+ * @author Jeremy Jones
  *
  * @property country
- * @property countryCode
- * @property slug
  * @property date
  * @property newConfirmed
  * @property totalConfirmed
+ * @property newDeaths
  * @property totalDeaths
+ * @property newRecovered
  * @property totalRecovered
  */
 data class CountryCovid(
+
     val country: String,
-    val countryCode: String,
-    val slug: String,
+    @SerializedName("updated")
     val date: String,
+    @SerializedName("todayCases")
     val newConfirmed: Int,
+    @SerializedName("cases")
     val totalConfirmed: Int,
+    @SerializedName("todayDeaths")
+    val newDeaths: Int,
+    @SerializedName("deaths")
     val totalDeaths: Int,
+    @SerializedName("todayRecovered")
+    val newRecovered: Int,
+    @SerializedName("recovered")
     val totalRecovered: Int
 )
