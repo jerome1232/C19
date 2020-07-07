970a5f7b906cb39358a37c7e76ddd867253ec2dc
jerome1232 <j.jones1232@gmail.com>
2020-07-07 17:58:32 -0600

    skeleton of CovidHistStateManager.kt created

---
+++ app/src/main/java/com/example/c19/model/CovidHistStateManager.kt
@@ -0,0 +1,33 @@
+package com.example.c19.model
+
+class CovidHistStateManager {
+    private val TAG = "CovidHistStateManager"
+
+    companion object {
+        var state = mutableListOf<StateUsCovidHistorical>()
+        var states = mutableListOf<MutableList<StateUsCovidHistorical>>()
+    }
+
+    fun getState(stateName: String): MutableList<StateUsCovidHistorical> {
+        if (states.isEmpty()) {
+                loadFromDisk()
+            }
+        
+        state = searchList(stateName)
+        if (!state.isEmpty()) return state
+        state = apiFetch(stateName)
+        return mutableListOf<StateUsCovidHistorical>()
+    }
+
+    private fun searchList(stateName: String): MutableList<StateUsCovidHistorical> {
+        TODO("Not yet implemented")
+    }
+
+    private fun loadFromDisk(): List<StateUsCovidHistorical> {
+        TODO("Not yet implemented")
+    }
+
+    private fun apiFetch(stateName: String): MutableList<StateUsCovidHistorical> {
+        TODO("Not yet implemented")
+    }
+}
