81853875d8726fba96a43f1eec46b820d3d032fe
Jeremy Jones <j.jones1232@gmail.com>
2020-07-14 16:57:29 -0600

    Optimized Imports in CovidApi.kt

--- app/src/main/java/com/example/c19/model/CovidApi.kt
+++ app/src/main/java/com/example/c19/model/CovidApi.kt
@@ -1,8 +1,6 @@
 package com.example.c19.model
 
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
